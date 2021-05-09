c5232a4b797264bf786a961d374d262114102760
valik-kinakh <valik.kinah@gmail.com>
2021-05-09 20:44:32 +0300

    Homework 12 is done!

---
+++ Homework12/Task1/server.js
@@ -0,0 +1,16 @@
+const http=require('http');
+const url=require('url');
+const static=require('node-static');
+const file=new static.Server('.',{cache:0});
+
+function accept(req,res) {
+    if (req.url=='/data.txt') {
+        file.serve(req,res);
+    }else
+    {
+        file.serve(req,res);
+    }
+}
+
+http.createServer(accept).listen(5000);
+console.log("Server running at http://localhost:5000");
